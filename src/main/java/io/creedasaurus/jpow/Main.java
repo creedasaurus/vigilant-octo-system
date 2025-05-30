82fa5ff3aa7c7772f2d0678ab8353b1acd2449c2
Creed Haymond <creedasaurus@gmail.com>
2025-05-30 11:57:35 -0600

    add old dynamo dependency

--- src/main/java/io/creedasaurus/jpow/Main.java
+++ src/main/java/io/creedasaurus/jpow/Main.java
@@ -6,8 +6,8 @@ import io.helidon.logging.common.LogConfig;
 import io.helidon.config.Config;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
-
-
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 
 /**
@@ -25,10 +25,16 @@ public class Main {
 
     /**
      * Application main entry point.
+     *
      * @param args command line arguments.
      */
     public static void main(String[] args) {
-        
+        final var credentials = DefaultCredentialsProvider.builder()
+                .asyncCredentialUpdateEnabled(true)
+                .build();
+        final var ddb = DynamoDbClient.builder()
+                .credentialsProvider(credentials)
+                .build();
         // load logging configuration
         LogConfig.configureRuntime();
 
@@ -45,7 +51,8 @@ public class Main {
 
 
         System.out.println("WEB server is up! http://localhost:" + server.port() + "/simple-greet");
-
+        ddb.close();
+        credentials.close();
     }
 
 
@@ -54,7 +61,7 @@ public class Main {
      */
     static void routing(HttpRouting.Builder routing) {
         routing
-               .register("/greet", new GreetService())
-               .get("/simple-greet", (req, res) -> res.send("Hello World!")); 
+                .register("/greet", new GreetService())
+                .get("/simple-greet", (req, res) -> res.send("Hello World!"));
     }
 }
